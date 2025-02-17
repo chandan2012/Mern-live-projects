fa5c501beb16491dc4eb971ecd6fce186ebbfede
Chandanshaw004 <107836055+chandan2012@users.noreply.github.com>
2025-02-17 18:54:38 +0530

    all clean

--- CurrencyConverter/Back-end/routers/currencyConverterRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const currencyConverterRouter = express.Router();
-const currencyConverterController = require("../controllers/currencyConverterController");
-
- currencyConverterRouter.post("/convert", currencyConverterController.convertCurrency);
-
-module.exports = currencyConverterRouter;
