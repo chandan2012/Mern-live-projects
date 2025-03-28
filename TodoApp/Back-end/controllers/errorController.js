a2c6d8e2708cabde6c5bad86983c1a2c44c380c2
Chandanshaw004 <107836055+chandan2012@users.noreply.github.com>
2025-03-28 09:53:35 +0530

    clean

--- TodoApp/Back-end/controllers/errorController.js
+++
@@ -1,4 +0,0 @@
-exports.get404 = (req, res, next) => {
-  console.log(req);
-  res.status(404).json({ message: "Page not found" });
-};
--- TodoApp/Front-end/src/utils/ModelUtil.js
+++
@@ -1,8 +0,0 @@
-export const todoItemToClientModel = (serverItem) => {
-  return {
-    id: serverItem._id,
-    todoText: serverItem.task,
-    todoDate: serverItem.date,
-    completed: serverItem.completed,
-  }
-}
